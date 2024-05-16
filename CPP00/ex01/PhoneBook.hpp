6d4f0d5df0d4c8ea672333cec423b07a914854f3
Stephie <sdemaude@student.42lehavre.fr>
2024-05-16 16:58:25 +0200

    hpp fix

--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -6,11 +6,15 @@
 /*   By: sdemaude <sdemaude@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 13:07:02 by sdemaude          #+#    #+#             */
-/*   Updated: 2024/05/15 17:41:13 by sdemaude         ###   ########.fr       */
+/*   Updated: 2024/05/16 16:57:22 by sdemaude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
 
 class PhoneBook {
 	private :
